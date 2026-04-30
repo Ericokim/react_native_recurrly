02a9162ffd4d6a61657bf96c2c6c60a98042c984
Eric <erickims08@gmail.com>
2026-04-30 22:11:53 +0300

    feat(fonts): add custom fonts and implement font loading in RootLayout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import "@/global.css";
 import { Link } from "expo-router";
 import { styled } from "nativewind";
 import { Text } from "react-native";
@@ -11,41 +10,25 @@ a breakdown of what the component is doing: */
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-sans-extrabold ">Home</Text>
+
       <Link
         href="/onboarding"
-        className="text-white mt-4 rounded-lg bg-primary p-4"
+        className="text-white mt-4 font-sans-bold rounded-lg bg-primary p-4"
       >
         Go to Onboarding
       </Link>
       <Link
         href="/(auth)/sign-in"
-        className="text-white mt-4 rounded-lg bg-primary p-4"
+        className="text-white mt-4 font-sans-bold rounded-lg bg-primary p-4"
       >
         Go to Sign In
       </Link>
       <Link
         href="/(auth)/sign-up"
-        className="text-white mt-4 rounded-lg bg-primary p-4"
+        className="text-white mt-4 font-sans-bold rounded-lg bg-primary p-4"
       >
         Go to Sign Up
-      </Link>
-      <Link
-        href="/subscriptions/spotify"
-        className="text-white mt-4 rounded-lg bg-primary p-4"
-      >
-        Spotify Subscriptions
-      </Link>
-      <Link
-        href={{
-          pathname: "/subscriptions/[id]",
-          params: { id: "claude" },
-        }}
-        className="text-white mt-4 rounded-lg bg-primary p-4"
-      >
-        Claude Max Subscriptions
       </Link>
     </SafeAreaView>
   );
